687ea3599f52cc18773cd527b8f387bc15553a58
chris-plonka <info@krzysztofplonka.pl>
2024-11-24 19:50:20 +0100

    ChatDetails remove pb-20

--- components/ChatDetails.jsx
+++ components/ChatDetails.jsx
@@ -113,70 +113,68 @@ const ChatDetails = ({ chatId }) => {
   return loading ? (
     <Loader />
   ) : (
-    <div className="pb-20">
-      <div className="chat-details">
-        <div className="chat-header">
-          {chat?.isGroup ? (
-            <>
-              <Link href={`/chats/${chatId}/group-info`}>
-                <img src={chat?.groupPhoto || "/assets/group.png"} alt="group-photo" className="profilePhoto" />
-              </Link>
+    <div className="chat-details">
+      <div className="chat-header">
+        {chat?.isGroup ? (
+          <>
+            <Link href={`/chats/${chatId}/group-info`}>
+              <img src={chat?.groupPhoto || "/assets/group.png"} alt="group-photo" className="profilePhoto" />
+            </Link>
 
-              <div className="text">
-                <p>
-                  {chat?.name} &#160; &#183; &#160; {chat?.members?.length} members
-                </p>
-              </div>
-            </>
-          ) : (
-            <>
-              <img src={otherMembers[0].profileImage || "/assets/person.jpg"} alt="profile photo" className="profilePhoto" />
-              <div className="text">
-                <p>{otherMembers[0].username}</p>
-              </div>
-            </>
-          )}
-        </div>
+            <div className="text">
+              <p>
+                {chat?.name} &#160; &#183; &#160; {chat?.members?.length} members
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <img src={otherMembers[0].profileImage || "/assets/person.jpg"} alt="profile photo" className="profilePhoto" />
+            <div className="text">
+              <p>{otherMembers[0].username}</p>
+            </div>
+          </>
+        )}
+      </div>
 
-        <div className="chat-body">
-          {chat?.messages?.map((message, index) => (
-            <MessageBox key={index} message={message} currentUser={currentUser} />
-          ))}
-          <div ref={bottomRef} />
-        </div>
+      <div className="chat-body">
+        {chat?.messages?.map((message, index) => (
+          <MessageBox key={index} message={message} currentUser={currentUser} />
+        ))}
+        <div ref={bottomRef} />
+      </div>
 
-        <div className="send-message">
-          <div className="prepare-message">
-            <CldUploadButton options={{ maxFiles: 1 }} onUpload={sendPhoto} uploadPreset="qnanoqg2">
-              <AddPhotoAlternate
-                sx={{
-                  fontSize: "35px",
-                  color: "#737373",
-                  cursor: "pointer",
-                  "&:hover": { color: "red" },
-                }}
-              />
-            </CldUploadButton>
-
-            <input
-              type="text"
-              placeholder="Write a message..."
-              className="input-field"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              onKeyDown={async (e) => {
-                if (e.key === "Enter" && !e.shiftKey) {
-                  e.preventDefault()
-                  sendText()
-                }
+      <div className="send-message">
+        <div className="prepare-message">
+          <CldUploadButton options={{ maxFiles: 1 }} onUpload={sendPhoto} uploadPreset="qnanoqg2">
+            <AddPhotoAlternate
+              sx={{
+                fontSize: "35px",
+                color: "#737373",
+                cursor: "pointer",
+                "&:hover": { color: "red" },
               }}
-              required
             />
-          </div>
+          </CldUploadButton>
 
-          <div onClick={sendText}>
-            <img src="/assets/send.jpg" alt="send" className="send-icon" />
-          </div>
+          <input
+            type="text"
+            placeholder="Write a message..."
+            className="input-field"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            onKeyDown={async (e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault()
+                sendText()
+              }
+            }}
+            required
+          />
+        </div>
+
+        <div onClick={sendText}>
+          <img src="/assets/send.jpg" alt="send" className="send-icon" />
         </div>
       </div>
     </div>
